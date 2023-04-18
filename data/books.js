e0418e1f96519dbed0773a9acda9c57f7262ecd9
ACER <210220@softwarica.edu.np>
2023-04-18 11:36:21 +0545

    books with json

---
+++ data/books.js
@@ -0,0 +1,14 @@
+const books =[
+    {
+        id: 1,
+        title: "JS Fundamentals",
+        author: "Manju R. Jyaps"
+    },
+    {
+        id: 1,
+        title: "War and Peace ",
+        author: "Ragnar Lothbruk"
+    }
+]
+
+module.exports = books
