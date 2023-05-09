4af5abc8c83c18deeb0d07f4481708efadd3306a
ACER <210220@softwarica.edu.np>
2023-05-09 10:45:12 +0545

    asd

--- index.js
+++ index.js
@@ -3,43 +3,44 @@ const express = require('express')
 
 const mongoose = require('mongoose')
 const books_routes = require('./routes/books-routes')
-const user_routes=require('./routes/user-routes')
+const user_routes = require('./routes/user-routes')
+const {verifyUser} = require('./middlewares/auth')
+
 const port = process.env.PORT
 mongoose.connect('mongodb://127.0.0.1:27017/Books-Review')
-
-
- .then(()=>{31333333333
-
-console.log('connected to the mongodb database server')
+.then(()=>{
+    console.log('connected to the mongodb database server')
 
 })
 
 .catch((err) => console.log(err))
+const app = express() //initiate  express by giving name app
+app.use(express.json());
+app.get('/', (req,res) =>{      //request pass through this
 
-const app = express() //initiate  express by giving name app
-app.use(express.json()) //request pass through this
+ //  console.log(req)
 
-app.get('/', (req,res) =>{
-
-//  console.log(req)
-
- res.send("babayaga")
-
+res.send("Hello World")
 })
 
+app.use('/users', user_routes)
+app.use(verifyUser)            //call middleware
 app.use('/books', books_routes )
 
 //Error handling middleware
-app.use((err, req ,res ,next)=> {
+app.use((err, req, res, nesxt)=>{
     console.error(err)
-    if (err.name==='ValidationError')res.status(400)
-    else if (err.name==='CastError')res.status(400)
+    if(err.name === "validationError") res.status (400)
+    else if(err.name ==="CastError") res.status(400)
     console.log(err.message)
-    res.json({error:err.message}) 
-    
+    res.json({error: err.message})
 })
 
 
+//unknown path 
+app.use((req,res)=> {
+    res.status(404).json({error: "Path not Found"})
+})
 app.listen(port,()=>{
-    console.log(`server is running at port ${port}`)
+    console.log('server is running at port ${port}')
 })
